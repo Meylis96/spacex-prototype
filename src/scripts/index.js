1aafc09f63412331bcbbac05420a53dd788282e6
Meylis <meylis.muhamedov96@gmail.com>
2020-08-05 13:10:02 +0300

    animations added

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+var instance = new vidbg('.video', {
+    mp4: './src/video/world.mp4', // URL or relative path to MP4 video
+    webm: './src/video/world.webm', // URL or relative path to webm video
+    poster: './src/img/earth.jpg', // URL or relative path to fallback image
+    overlay: false
+  });
+
+var rellax = new Rellax('.rocket-img');
